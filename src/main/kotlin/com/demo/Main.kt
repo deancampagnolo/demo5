022fc7e3701be855c4c4a05e1e317dc0b21b9970
Dean Campagnolo <deancampagnolo@gmail.com>
2022-12-01 17:55:29 -0800

    Added / endpoint

--- src/main/kotlin/com/demo/Main.kt
+++ src/main/kotlin/com/demo/Main.kt
@@ -2,9 +2,17 @@ package com.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@RestController
 open class Main {
+    @GetMapping("/")
+    public fun greet(): String {
+        return "Hello"
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
