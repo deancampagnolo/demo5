60b2a5fbd6c5520497b05be6950194c8e3ef3f52
Dean Campagnolo <deancampagnolo@gmail.com>
2022-12-01 17:44:46 -0800

    Starting from scratch

---
+++ src/main/kotlin/com/demo/Main.kt
@@ -0,0 +1,14 @@
+package com.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<Main>(*args)
+        }
+    }
+}
